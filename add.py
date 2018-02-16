60403e723c24bf2e3e97ec346ec027e45c4eec03
sridharnk52 <sridharnk52@gmail.com>
2018-02-16 07:32:18 +0530

    version 02

---
+++ add.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 07:56:40 2018
+
+@author: Sridhar
+"""
+a=20
+b=30
+print(a+b)
+
+
+    
+     
+    
+    
+    
+    
+    
+    
+    
+
+    
